4934b4038e82248cd85e187662900f763fca0b7b
Andres Riofrio <riofrios@gmail.com>
2012-12-12 20:40:30 -0800

    Add git prompt information

--- bashrc.d/prettyness.sh
+++ bashrc.d/prettyness.sh
@@ -1,32 +1,78 @@
 
 # set a fancy prompt (non-color, unless we know we "want" color)
 case "$TERM" in
-    xterm-color) color_prompt=yes;;
+  xterm-color) color_prompt=yes;;
 esac
 if [ -x /usr/bin/tput ] && tput setaf 1 >&/dev/null; then
-    # We have color support; assume it's compliant with Ecma-48
-    # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
-    # a case would tend to support setf rather than setaf.)
-    color_prompt=yes
+  # We have color support; assume it's compliant with Ecma-48
+  # (ISO/IEC-6429). (Lack of such support is extremely rare, and such
+  # a case would tend to support setf rather than setaf.)
+  color_prompt=yes
 else
-    color_prompt=
+  color_prompt=
 fi
 
+git_info() {
+  branch=$(git symbolic-ref --short HEAD 2> /dev/null)
+  if [ -z $branch ]; then # detached head
+    branch=$(git name-rev --name-only HEAD 2> /dev/null)
+  fi
+
+  staged=$(git diff --staged --name-status 2> /dev/null | wc -l)
+  unstaged=$(git diff --name-status 2> /dev/null | wc -l)
+  untracked=$(git status --porcelain 2> /dev/null |
+    grep '^?? ' | wc -l)
+  unmerged=$(git status --porcelain 2> /dev/null |
+    grep '^(U.|.U) ' | wc -l)
+
+  total=$(git status --porcelain 2> /dev/null | wc -l)
+  unknown=$[ $total - $staged - $unstaged - $untracked - $unmerged ]
+
+  if [ $staged -gt 0 ] || [ $unstaged -gt 0 ] ||
+  [ $unmerged -gt 0 ] || [ $untracked -gt 0 ] ||
+  [ $unknown -ne 0 ]; then
+    numbers=1
+  else
+    numbers=
+  fi
+
+  if [ -n "$branch" ] || [ -n "$numbers" ]; then
+    echo -n ' ('
+
+    [ -n "$branch" ] && echo -n $branch
+    [ -z "$branch" ] && echo -n "none"
+
+    tput bold
+    [ $staged -gt 0 ] && echo -n " •$staged"
+    [ $unstaged -gt 0 ] && echo -n " +$unstaged"
+    [ $unmerged -gt 0 ] && echo -n " x${unmerged}"
+    [ $untracked -gt 0 ] && echo -n " !$untracked"
+    [ $unknown -ne 0 ] && echo -n " _$unknown"
+    tput sgr0
+
+    echo -n ')'
+  fi
+}
+
 if [ "$color_prompt" = yes ]; then
-    PS1='${debian_chroot:+($debian_chroot)}\[\e[34m\]\u@\h\[\e[00m\] : \[\e[1;32m\]\w\[\e[00m\]\n> '
+  PS1='${debian_chroot:+($debian_chroot)}'
+  PS1+='\[\e[34m\]\u@\h\[\e[00m\] : '
+  PS1+='\[\e[1;32m\]\w\[\e[00m\]'
+  PS1+='$(git_info)'
+  PS1+='\n> '
 else
-    PS1='${debian_chroot:+($debian_chroot)}\u@\h : \w\n> '
+  PS1='${debian_chroot:+($debian_chroot)}\u@\h : \w\n> '
 fi
 unset color_prompt force_color_prompt
 
 # enable color support of ls and also add handy color-enabled aliases
 if [ -x /usr/bin/dircolors ]; then
-    test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
-    alias ls='ls --color=auto'
-    #alias dir='dir --color=auto'
-    #alias vdir='vdir --color=auto'
+  test -r ~/.dircolors && eval "$(dircolors -b ~/.dircolors)" || eval "$(dircolors -b)"
+  alias ls='ls --color=auto'
+  #alias dir='dir --color=auto'
+  #alias vdir='vdir --color=auto'
 
-    alias grep='grep --color=auto'
-    alias fgrep='fgrep --color=auto'
-    alias egrep='egrep --color=auto'
+  alias grep='grep --color=auto'
+  alias fgrep='fgrep --color=auto'
+  alias egrep='egrep --color=auto'
 fi
