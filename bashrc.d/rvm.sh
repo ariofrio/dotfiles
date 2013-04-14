65c73b9f34d57bbe1efe655beb048dc61fe6d4e0
Andres Riofrio <riofrios@gmail.com>
2013-04-13 19:20:06 -0700

    Add RVM bin directory to PATH

--- bashrc.d/rvm.sh
+++ bashrc.d/rvm.sh
@@ -1,1 +1,5 @@
-[[ -s "/home/ariofrio/.rvm/scripts/rvm" ]] && source "/home/ariofrio/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
+# Add RVM to PATH for scripting
+PATH=$PATH:$HOME/.rvm/bin
+
+# Load RVM into a shell session *as a function*
+[[ -s "/home/ariofrio/.rvm/scripts/rvm" ]] && source "/home/ariofrio/.rvm/scripts/rvm" 
